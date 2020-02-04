99afc7d6f1dbab453d5392316b13fccb9ad19fa0
dominik47 <df6998@g.rit.edu>
2020-02-04 17:55:26 +0100

    Updated the board and added new colored ludo pieces.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,18 +2,56 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.SVGPath;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Test");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage theStage) throws Exception {
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        primaryStage.setTitle("Test");
+//        primaryStage.setScene(new Scene(root, 300, 275));
+//        primaryStage.show();
+
+        theStage.setTitle( "Canvas Example" );
+        SVGPath svgPath = new SVGPath();
+
+        String path = "M 100 100 L 300 100 L 200 300 z";
+
+        //Setting the SVGPath in the form of string
+        svgPath.setContent(path);
+
+        Group root = new Group(svgPath);
+        Scene theScene = new Scene( root );
+        theStage.setScene( theScene );
+
+        Canvas canvas = new Canvas( 400, 200 );
+        root.getChildren().add( canvas );
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.setFill( Color.RED );
+        gc.setStroke( Color.BLACK );
+        gc.setLineWidth(2);
+        Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 48 );
+        gc.setFont( theFont );
+        gc.fillText( "Hello, World!", 60, 50 );
+        gc.strokeText( "Hello, World!", 60, 50 );
+
+//        Image earth = new Image( "earth.png" );
+//        gc.drawImage( earth, 180, 100 );
+
+        theStage.show();
     }
 
 
